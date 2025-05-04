ee63cde1198575b00651f4170ba6caa275231017
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2025-05-04 17:47:09 -0300

    chore: modify build output and exports for sdk

--- packages/client/tsup.config.ts
+++ packages/client/tsup.config.ts
@@ -2,7 +2,10 @@ import { type Options, defineConfig } from "tsup"
 
 export default defineConfig((options: Options) => ({
 	entry: ["src/index.ts"],
-	format: ["cjs", "esm"],
+	format: ["esm"],
+	target: "esnext",
+	splitting: true,
+	minify: true,
 	dts: true,
 	treeshake: true,
 	clean: true,
